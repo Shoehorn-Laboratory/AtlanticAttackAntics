861c0cabfe4b705add42cf9c6f8423e7c660e24a
Theo Lee <th3o1237@gmail.com>
2025-02-01 00:20:03 -0500

    holy

--- main.cpp
+++ main.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
+#include <cstdint>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <functional>
+#include <ranges>
 
-int main()
-{
-	std::cout << "Hi Mom!";
-	int x;
-	std::cin >> x;
+int main() {
+	std::vector v{1, 2, 3, 4, 5};
+	for (const auto& x : v | std::views::transform([](const auto& x) { return x + 1; }) | std::views::filter([](const auto& x){ return x % 2 == 0; })) {
+		std::cout << x << '\n';
+	}
 }
